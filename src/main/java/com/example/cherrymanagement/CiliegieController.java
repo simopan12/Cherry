c428ae933a27ed97df5c0a21f4d6c069593ac660
simopan12 <panellasimone02@gmail.com>
2023-06-28 12:27:29 +0200

    Third commit

--- src/main/java/com/example/cherrymanagement/CiliegieController.java
+++ src/main/java/com/example/cherrymanagement/CiliegieController.java
@@ -3,9 +3,10 @@ package com.example.cherrymanagement;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class CiliegieController {
-
+    public void stampa(void){
+        System.out.println("ciao");
+    }
 }
